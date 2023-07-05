cbbd434fa125cd429741900a446e24f48c1cc3f3
colin11702 <colinmcherney@gmail.com>
2023-07-04 20:54:04 -0700

    JavaScript Practice #3 Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let length = str.length;
+  let letters = "";
 
+  for (i = 0; length > i; i++) {
+    letters = str[i] + letters;
+  }
+  return letters;
 };
 
 // Do not edit below this line
