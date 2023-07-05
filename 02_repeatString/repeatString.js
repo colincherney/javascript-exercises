eeac441709bd2c1d4bbebc2b28378b39d0b2e056
colin11702 <colinmcherney@gmail.com>
2023-07-04 20:43:31 -0700

    Javascript Practice #2 Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let full = "";
 
+    for (let i = 0; i < num; i++) {
+      full = string + full;
+    }
+    return full;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
