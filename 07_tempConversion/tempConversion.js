d15a542f61548e7e085d58bb7dd47e60dfd3407d
colin11702 <colinmcherney@gmail.com>
2023-07-05 20:19:39 -0700

    JavaScript Practice #7 Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let first = temp - 32;
+  let second = first * (5 / 9);
+  let round = second.toFixed(1);
+  let convert = parseFloat(round);
+  return convert;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let equation = temp * (9 / 5) + 32;
+  let round = equation.toFixed(1);
+  let convert = parseFloat(round);
+  return convert;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
