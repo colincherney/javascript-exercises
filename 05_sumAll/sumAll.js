028af163030ffc6bcc7941a2ecf03761ffbb4ddf
colin11702 <colinmcherney@gmail.com>
2023-07-05 19:35:00 -0700

    JavaScript Practice #5 Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (Number.isInteger(num1) && Number.isInteger(num2)) {
+    if (num1 < num2) {
+      let difference = num2 - num1 + 1;
+      let startingNum = num1;
+      for (i = startingNum; i <= difference; i++) {
+        sum = sum + i;
+      }
+      return sum;
+    }
+    if (num2 < num1) {
+      let difference = num1 - num2 + 1;
+      let startingNum = num2;
+      for (i = startingNum; i <= difference; i++) {
+        sum = sum + i;
+      }
+      return sum;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
