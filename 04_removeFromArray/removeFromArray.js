19c9dba49d456e1917b92fb1bea6b613d48c3a0b
colin11702 <colinmcherney@gmail.com>
2023-07-05 19:07:20 -0700

    JavaScript Practice #4 Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+  let Array = arguments[0];
+  for (i = 1; arguments.length > i; i++) {
+    let index = Array.indexOf(arguments[i]);
+    if (index > -1) {
+      Array.splice(index, 1);
+    } else {
+      console.log("Value not in array!");
+    }
+  }
+  return Array;
 };
 
 // Do not edit below this line
